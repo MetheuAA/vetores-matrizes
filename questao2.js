5a8227f56c249e54c638982decb96af8f8a3f085
MetheuAA <matheo200817@gmail.com>
2024-04-10 10:58:41 -0400

    add questao 2

---
+++ questao2.js
@@ -0,0 +1,10 @@
+//Questão 2: Crie um código em JavaScript para adicionar um elemento no final de um vetor.
+
+let meuArray = [];
+
+//Adicionando elementos ao final do array
+meuArray.push(1);
+meuArray.push(2);
+meuArray.push(3);
+meuArray.push(4);
+meuArray.push(5);
